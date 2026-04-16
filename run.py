9fc50e9bb93530569f789fe757950d3fed4e52be
uzalisher <uzalisher3@gmail.com>
2026-04-16 23:34:17 +0900

    feat: python system variables & special methods via dunder

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print("Hello World")
+# Dunder __builtins__ , __init__
+massage = "PYTHON: Everything is object"
+print(massage)
+
+result = type(massage)
+print("result:", result)
+
+''' in Python, there are bulitin tools:
+
+(1) TYPES > int float str list dict
+(2) Function > print(), len(), input(), type()
+(3) CONSTANT > True False None
+ 
+'''
+print(dir(__builtins__))
