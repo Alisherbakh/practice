c0c9c7f71f44f05a879a50f770172f65cadcac1a
uzalisher <uzalisher3@gmail.com>
2026-04-23 19:59:57 +0900

    feat: learn tuple and list

---
+++ tuples.py
@@ -0,0 +1,30 @@
+''' Tuples
+   (1) What is tuples: type vs list
+   (2) Unpacking arguments
+   (3) zip
+
+'''
+
+print("===== What is tuples: type vs list ======")
+# Java/PHP/NodeJS array => Python list
+
+# literal
+numbs = [3, 5, 1, 2]
+
+
+# constructor
+letter = list("Hello World!")
+
+fruits = ["apple", "lemon", "banana", "kiwi"]
+print("before fruit:", fruits)
+
+fruits[2] = "melon"
+print("after fruits:", fruits)
+
+# we can not mutate tuple
+
+animals = ("dog", "cat", "fish", "lion")
+tuple_obj = ("MIT", 100, True, None)
+
+print(animals[0])
+# animals[0] = "bird"
