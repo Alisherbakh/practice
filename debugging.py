b411a5c9a941059a19d8d75ccc7a858aaa69b21c
uzalisher <uzalisher3@gmail.com>
2026-04-29 07:44:10 +0900

    feat: debugging process

--- debugging.py
+++ debugging.py
@@ -12,11 +12,11 @@ print(" ======= Python Packages & Core Packages ======")
 
 
 # Core
-# t = turtle.Turtle()
-# t.shape("turtle")
-# t.speed(2)
-# t.circle(150)
-# turtle.done()
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(2)
+t.circle(150)
+turtle.done()
 
 print("======")
 
@@ -49,3 +49,18 @@ with Image.open("material/tom3.png") as img_obj:
     resized_img = img_obj.resize((200, 200))
     resized_img.show()
     resized_img.save("material/sample.png")
+
+
+print(" ======= Debugging ======")
+
+
+def get_summary(*args):  # Define
+    total_amount = 0
+    for a in args:
+        total_amount += a
+        return total_amount  # find bug via debugging
+
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)  # Call
+print("Result:", result)
