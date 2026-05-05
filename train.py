a45bc52a77c2b26dd418da248e6eb519bb1123c4
uzalisher <uzalisher3@gmail.com>
2026-05-05 18:31:38 +0900

    feat: MITASK-I

--- train.py
+++ train.py
@@ -1,3 +1,23 @@
+# I-TASK (PYTHON)
+
+# Shunday function tuzing, unga string argument pass bolsin.
+# Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+# MASALAN: get_digits("m14i1t") return qiladi "141"
+# Yechim :
+
+
+def get_num(arg):
+    numb = []
+    for b in arg:
+        if b.isdigit():
+            numb.append(b)
+    return "".join(numb)
+
+
+result = get_num("m14i1t")
+print("result:", result)
+
+
 # G-TASK (PYTHON)
 
 # Shunday function tuzingki unga integerlardan iborat array pass bolsin va
@@ -5,11 +25,11 @@
 # MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
 
-def get_highest(num):
-    a = max(num)
-    b = num.index(a)
-    return f"index:{b}  value:{a}"
+# def get_highest(num):
+#   a = max(num)
+#    b = num.index(a)
+#    return f"index:{b}  value:{a}"
 
 
-result = get_highest([5, 21, 12, 21, 8])
-print("result:", result)
+# result = get_highest([5, 21, 12, 21, 8])
+# print("result:", result)
